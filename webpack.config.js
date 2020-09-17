c7ee0641d1fc15183e8da92665b21a541c8a5c3c
Peter Leiva <pleiva@id.uff.br>
2020-09-17 17:01:32 -0300

    Install and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpackNodeExternals = require('webpack-node-externals');
+
+const { NODE_ENV: mode = 'development' } = { ...process.env };
+
+module.exports = {
+  target: 'node',
+  mode,
+  entry: './src/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['ts-loader', 'eslint-loader'],
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+
+  plugins: [new CleanWebpackPlugin()],
+  externals: [webpackNodeExternals()],
+  resolve: {
+    extensions: ['.tsx', '.ts'],
+    alias: {
+      ['@config']: path.join(__dirname, 'src', 'config'),
+      ['@lib']: path.join(__dirname, 'src', 'lib'),
+    },
+  },
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.join(__dirname, 'dist'),
+  },
+};
