e4f37847b9a7439c05c0da93952db18c648448c5
Peter Leiva <pleiva@id.uff.br>
2020-09-18 20:25:04 -0300

    Add reply model which represents topic comments

---
+++ src/topics/reply.model.ts
@@ -0,0 +1,37 @@
+import { Document, model, Schema } from 'mongoose';
+import { User, UserModel } from 'users';
+
+/**
+ * Represents a comment from a user to a specific topic.
+ *
+ * Replies can itself be replies by other users, so if constructs a tree
+ * structure of comments for a topic posted by some user.
+ */
+export class Reply {
+  author!: User;
+  repliedTo?: Reply;
+  createdAt!: Date;
+  updatedAt!: Date;
+}
+
+export const ReplySchema = new Schema(
+  {
+    authorId: {
+      type: Schema.Types.ObjectId,
+      ref: UserModel,
+      required: true,
+    },
+
+    repliedToId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Reply',
+    },
+  },
+  { timestamps: true }
+);
+
+export type ReplyDocument = Reply & Document;
+
+export const ReplyModel = model<ReplyDocument>('Reply', ReplySchema);
+
+export default ReplyModel;
