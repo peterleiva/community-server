e7a0e97ac57a63830fa99b6dafd1ef16efec9401
Peter Leiva <pleiva@id.uff.br>
2020-09-19 23:30:51 -0300

    Separate tsconfig between test and build options and config jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,22 @@
 module.exports = {
   preset: 'ts-jest',
-	testEnvironment: 'node',
-	collectCoverage: true
+  globals: {
+    'ts-jest': {
+      tsConfig: './tsconfig.test.json',
+    },
+  },
+
+  testEnvironment: 'node',
+  collectCoverage: true,
+  moduleDirectories: ['node_modules', 'src', 'test'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/test/', '/(.*).d.ts/'],
+  moduleNameMapper: {
+    '^src/(.*)$': ['<rootDir>/src/$1'],
+    '^test/(.*)$': ['<rootDir>/test/$1'],
+    '^factories/(.*)$': ['<rootDir>/test/factories/$1']
+  },
+  setupFilesAfterEnv: [
+    'jest-extended',
+    '<rootDir>/test/utils/matchers/index.ts',
+  ],
 };
