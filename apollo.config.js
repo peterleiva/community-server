88ccb5f17bcb7e9e1407070a92f72d5cdca32579
Peter Leiva <pleiva@id.uff.br>
2020-09-20 00:19:52 -0300

    Run prettier to apollo.config.js

--- apollo.config.js
+++ apollo.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	client: {
-		service: 'Community',
-		url: 'http://localhost:3000/api'
-	}
-}
+  client: {
+    service: 'Community',
+    url: 'http://localhost:3000/api',
+  },
+};
