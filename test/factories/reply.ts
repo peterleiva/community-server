e648d21dd542070c6fcca33aa93c9ccdccd7a39b
Peter Leiva <pleiva@id.uff.br>
2020-09-23 20:21:04 -0300

    Use optional after creation from reply author and topic

--- test/factories/reply.ts
+++ test/factories/reply.ts
@@ -8,12 +8,15 @@ import TopicFactory from './topic';
 export default Factory.define('reply')
   .sequence('_id', () => new Types.ObjectId())
   .after(async (reply) => {
-    // cria um topico, cria um autor
-    const author = await UserModel.create(UserFactory.build());
-    reply.author = author._id;
+    if (!reply.author) {
+      const author = await UserModel.create(UserFactory.build());
+      reply.author = author._id;
+    }
 
-    const topic = await TopicModel.create(await TopicFactory.build());
-    reply.topic = topic._id;
+    if (!reply.topic) {
+      const topic = await TopicModel.create(await TopicFactory.build());
+      reply.topic = topic._id;
+    }
 
     return reply;
   });
