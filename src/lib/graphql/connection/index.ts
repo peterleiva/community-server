b510e1a02322280a7c0266cdf6d3c9aca4067561
Peter Leiva <pleiva@id.uff.br>
2021-02-16 19:16:05 -0300

    Fix cursor resolver export name

--- src/lib/graphql/connection/index.ts
+++ src/lib/graphql/connection/index.ts
@@ -1,5 +1,5 @@
 export { default as ConnectionSchema } from './connection.schema';
-export { resolver as CursorResolve } from './cursor-scalar';
+export { resolver as CursorResolver } from './cursor-scalar';
 
 export { default as Connection } from './connection';
 export { default as Edge } from './edge';
