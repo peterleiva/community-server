0d6d27abe7ddfabbf1f9c3504423817b843ebdea
Peter Leiva <pleiva@id.uff.br>
2021-02-28 14:56:24 -0300

    Add webpack BannerPlugin

--- build-tools/webpack.production.js
+++ build-tools/webpack.production.js
@@ -1,7 +1,17 @@
+const webpack = require('webpack');
+
 /**
  * Webpack production environment
  *
  * @param {object} env
  * @returns import("webpack").Configuration
  */
-module.exports = (env) => ({});
+module.exports = env => ({
+  plugins: [
+    new webpack.BannerPlugin({
+      raw: true,
+      banner: '#!/usr/bin/env node',
+      entryOnly: true,
+    }),
+  ],
+});
