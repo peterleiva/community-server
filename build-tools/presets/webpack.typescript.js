d07938f99843f0dd564229b15a29c0dac35597a9
Peter Leiva <pleiva@id.uff.br>
2021-02-28 14:47:38 -0300

    Add typescript webpack preset
    Webpack typescript configuration file defines options specific to typescript

---
+++ build-tools/presets/webpack.typescript.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const ForkTsCheckerNotifierWebpackPlugin = require('fork-ts-checker-notifier-webpack-plugin');
+
+/** @type {string} Typescript build configuration file path */
+const rootPath = path.resolve(__dirname, '..', '..');
+const configFile = path.resolve(rootPath, 'tsconfig.build.json');
+
+/**
+ * Webpack production environment
+ *
+ * @param {object} env
+ * @returns import("webpack").Configuration
+ */
+module.exports = env => ({
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      eslint: { files: './src/**/*' },
+    }),
+    new ForkTsCheckerNotifierWebpackPlugin({
+      title: 'Community Server build',
+      excludeWarnings: true,
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.json'],
+    symlinks: false,
+    plugins: [new TsconfigPathsPlugin({ configFile })],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: path.resolve(rootPath, 'src'),
+        exclude: /__tests__/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile,
+            transpileOnly: true,
+          },
+        },
+      },
+    ],
+  },
+});
