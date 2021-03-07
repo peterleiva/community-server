36f530df84177f6d42be6ee2de93c94c866b9868
Peter Leiva <pleiva@id.uff.br>
2021-03-07 13:52:05 -0300

    Add scripts to build docker and run in interactive

--- build-tools/docker/build.sh
+++ build-tools/docker/build.sh
@@ -8,4 +8,27 @@
 # options when building the app image
 ##
 
-docker build $@ --tag community-server .
+getAbsolutePath() {
+	echo "$(
+		cd -- "$(dirname "$1")" >/dev/null 2>&1
+		pwd -P
+	)"
+}
+
+# take build script directory which is where Dockerfile lives to build them
+basedir=$(getAbsolutePath $0)
+rootdir=$(getAbsolutePath "$basedir/../../.")
+
+# Docker build variables
+APP_NAME="community-server"
+Dockerfile="$basedir/Dockerfile"
+tag=$APP_NAME
+
+# Information about docker building folders
+printf "\n\n"
+echo "Bulding Dockerfile at $Dockerfile"
+echo "Project at $rootdir"
+printf "\n\n"
+
+# uses rootdir as the docker context
+docker build $@ --tag $tag -f $Dockerfile $rootdir
---
+++ build-tools/docker/shell.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+###
+# Run docker container in shell mode
+#
+
+CONTAINER_NAME="community-server"
+TAG="latest"
+IMAGE="community-server:$TAG"
+SHELL="/bin/bash"
+
+# Remove the container with --rm when exists the iteratictive environment
+docker run --interactive --tty --rm --name $CONTAINER_NAME $IMAGE $SHELL
