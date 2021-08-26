80e1f76907ae22c501f9736efaa5398c66674f90
Peter <github@peter.dev.br>
2021-08-26 18:44:30 -0300

    ci: fix node cache

--- src/__tests__/main.ts
+++ src/__tests__/main.ts
@@ -1,1 +1,23 @@
-it.todo("All setup");
+import type { Connection } from "mongoose";
+import { databaseSetup } from "utils";
+
+const service = databaseSetup();
+
+it("All setup", async () => {
+	const db = (service.connection as Connection).db;
+
+	const movie = {
+		title: "True Romance",
+		year: "1993",
+	};
+
+	await db.collection("movies").insertOne(movie);
+	await expect(db.collection("movies").findOne()).resolves.toMatchObject(movie);
+});
+
+it("another", async () => {
+	const db = service.connection as Connection;
+	const movies = await db.collection("movies").find().toArray();
+
+	expect(movies).toBeEmpty();
+});
