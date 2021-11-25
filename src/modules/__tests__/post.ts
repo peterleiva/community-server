41a35d19a17cd3a80e953f4f5a28c51bf485381d
Peter <github@peter.dev.br>
2021-11-24 21:15:20 -0300

    WIP: test: post's unit test

--- src/modules/__tests__/post.ts
+++ src/modules/__tests__/post.ts
@@ -1,23 +1,67 @@
-import { PostFactory } from "factory";
+// import { Error } from "mongoose";
+import { PostFactory, UserFactory } from "factory";
 import { databaseSetup } from "utils";
+import type { PostDocument } from "../post";
+import { UserModel } from "modules/user";
 
 databaseSetup();
 
 describe("Post", () => {
-	it("creates post", async () => {
-		await expect(PostFactory.create()).toResolve();
+	let post: PostDocument;
+
+	beforeEach(async () => {
+		post = await PostFactory.create();
 	});
 
-	it.todo("can't change its author");
-	it.todo("auto-populate author");
-	it.todo("users can like once");
+	it("can't change its author", async () => {
+		const author = await UserFactory.create();
+		post.author = author._id;
+
+		await post.save();
+
+		expect(post.author._id).not.toBe(author._id);
+	});
+
+	it("auto-populate author", async () => {
+		const post = await PostFactory.create();
+
+		expect(post.author).toBeInstanceOf(UserModel);
+	});
+
+	it("users can like once", async () => {
+		const user = await UserFactory.create();
+
+		post.likedBy.push(user._id);
+		await post.save();
+
+		post.likedBy.push(user._id);
+
+		await expect(post.validate("likedBy")).rejects.toThrow(
+			/array must have unique values/
+		);
+	});
+
+	it("user can like different posts", async () => {
+		const user = await UserFactory.create();
+		post.likedBy.push(user.id);
+		await post.save();
+
+		const post2 = await PostFactory.create();
+		post2.likedBy.push(user.id);
+
+		await expect(post2.save()).not.toReject();
+	});
+
 	it.todo("message can have at most 10.000 characters");
 	it.todo("message can have at least 10 characters");
 
 	it.todo("children can't have cyclic dependency");
 
 	describe("#likes", () => {
-		it.todo("gives 0 when no one liked");
+		it("gives 0 when no one liked", () => {
+			expect(post.likes).toBe(0);
+		});
+
 		it.todo("gives the number of users who liked");
 	});
 });
