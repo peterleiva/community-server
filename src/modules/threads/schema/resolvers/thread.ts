7824ee5ab4a703c7da42a79c6a30d6f13dcb4785
Peter <github@peter.dev.br>
2021-12-04 20:15:16 -0300

    WIP: post's participants resolver

--- src/modules/threads/schema/resolvers/thread.ts
+++ src/modules/threads/schema/resolvers/thread.ts
@@ -1,71 +1,67 @@
 import type { IFieldResolver } from "@graphql-tools/utils";
-import { ThreadDocument } from "../../thread";
-import { PostModel, Post } from "modules/post";
-import type { User } from "modules/user";
+import type { ThreadDocument } from "../../thread";
 import type { PageArgs } from "lib/connection/types";
+import { PostModel, Post } from "modules/post";
 import Paginator from "modules/paginator";
+
+export type PostParticipantResult = {
+	page: Post[];
+	iteractions: Post[];
+};
 
 const participants: IFieldResolver<
 	ThreadDocument,
 	unknown,
 	PageArgs,
-	Promise<User[]>
+	Promise<PostParticipantResult>
 > = async function participants(source, args) {
 	const paginate = new Paginator(args);
-
-	const pipeline = [
-		{
-			$match: {
-				_id: source.op,
-			},
-		},
-		{
-			$graphLookup: {
-				from: "posts",
-				startWith: "$_id",
-				connectFromField: "children",
-				connectToField: "_id",
-				as: "replies",
-			},
-		},
-		{
-			$unwind: {
-				path: "$replies",
-			},
-		},
-		{
-			$match: {
-				"replies.updatedAt": {
-					$lt: paginate.after,
+	const result = await PostModel.aggregate<{
+		page: Post[];
+		metadata: { interactions: Post[] };
+	}>()
+		.match({ _id: source.op })
+		.graphLookup({
+			from: "posts",
+			startWith: "$_id",
+			connectFromField: "children",
+			connectToField: "_id",
+			as: "replies",
+		})
+		.facet({
+			metadata: [{ $project: { interactions: "$replies" } }],
+			page: [
+				{
+					$unwind: {
+						path: "$replies",
+					},
 				},
-			},
-		},
-		{
-			$sort: { "replies.updatedAt": -1 },
-		},
-		{
-			$limit: paginate.limit,
-		},
-		{
-			$graphLookup: {
-				from: "users",
-				startWith: "$replies.author",
-				connectFromField: "_id",
-				connectToField: "_id",
-				maxDepth: 1,
-				as: "participants",
-			},
-		},
-	];
+				{
+					$match: {
+						"replies.updatedAt": {
+							$gt: paginate.after,
+						},
+					},
+				},
+				{
+					$sort: { "replies.updatedAt": -1 },
+				},
+				{
+					$limit: paginate.limit,
+				},
+			],
+		})
+		.exec();
 
-	const result = await PostModel.aggregate<{ participants: User[] }>(
-		pipeline
-	).exec();
+	const {
+		page,
+		metadata: { interactions },
+	} = result;
 
-	const participants = result.flatMap(({ participants }) => participants);
-
-	// todo: keep only distinct participants
-	return participants;
+	return {
+		page,
+		interactions,
+	};
 };
 
 const post: IFieldResolver<
