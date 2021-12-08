5987275c605fa457e8fd90850350750c2d701d19
Peter <github@peter.dev.br>
2021-12-08 10:44:00 -0300

    test: thread factory with replies option

--- test/factory/thread.ts
+++ test/factory/thread.ts
@@ -2,26 +2,41 @@ import { Factory } from "fishery";
 import casual from "casual";
 import { ThreadDocument, Thread, ThreadModel } from "modules/threads";
 import PostFactory from "./post";
+import { PostDocument } from "modules/post";
 
-export default Factory.define<Partial<Thread>, never, ThreadDocument>(
-	({ onCreate, associations }) => {
-		onCreate(async thread => {
-			const op = associations?.op ?? (await PostFactory.create())?._id;
-			const threadDoc = new ThreadModel({ ...thread, op });
+interface ThreadTransientParams {
+	replies: number;
+}
 
-			await threadDoc.save();
+export default Factory.define<
+	Partial<Thread>,
+	ThreadTransientParams,
+	ThreadDocument
+>(({ onCreate, transientParams, associations }) => {
+	onCreate(async thread => {
+		const op = associations?.op ?? (await PostFactory.create())?._id;
+		const threadDoc = new ThreadModel({ ...thread, op });
+		const withReplies = transientParams?.replies ?? 0;
 
-			return threadDoc;
-		});
-
-		const thread: Partial<Thread> = {
-			title: casual.title,
-		};
-
-		if (associations?.op) {
-			thread.op = associations.op;
+		if (withReplies > 0) {
+			const thread = await threadDoc.populate<{ op: PostDocument }>("op");
+			const posts = await PostFactory.createList(withReplies);
+			thread.op.children.push(...posts.map(p => p._id));
+			await thread.op.save();
 		}
 
-		return thread;
+		await threadDoc.save();
+
+		return threadDoc;
+	});
+
+	const thread: Partial<Thread> = {
+		title: casual.title,
+	};
+
+	if (associations?.op) {
+		thread.op = associations.op;
 	}
-);
+
+	return thread;
+});
