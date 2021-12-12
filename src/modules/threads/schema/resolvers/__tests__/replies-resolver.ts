34063f939a22729f54deaad9345d68092f85ff9d
Peter <github@peter.dev.br>
2021-12-12 12:41:10 -0300

    test: replies post's resolver unit test

---
+++ src/modules/threads/schema/resolvers/__tests__/replies-resolver.ts
@@ -0,0 +1,35 @@
+import type { GraphQLResolveInfo } from "graphql";
+import { PostFactory, ThreadFactory } from "factory";
+import { replies } from "../thread";
+
+describe("replies resolver", () => {
+	test("number of post's op subtree", async () => {
+		const sublevel2 = await PostFactory.createList(2);
+		const sublevel1 = await PostFactory.create(
+			{},
+			{ associations: { children: sublevel2.map(p => p._id) } }
+		);
+
+		const op = await PostFactory.create(
+			{},
+			{ associations: { children: [sublevel1._id] } }
+		);
+
+		const thread = await ThreadFactory.create(
+			{},
+			{ associations: { op: op._id } }
+		);
+
+		await expect(
+			replies(thread, {}, null, {} as GraphQLResolveInfo)
+		).resolves.toBe(3);
+	});
+
+	test("no reply when op has no interaction", async () => {
+		const thread = await ThreadFactory.create();
+
+		await expect(
+			replies(thread, {}, null, {} as GraphQLResolveInfo)
+		).resolves.toBe(0);
+	});
+});
