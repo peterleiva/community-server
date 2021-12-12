33d5051c2eafe81494a7ecf296d8d22e3ddfdb27
Peter <github@peter.dev.br>
2021-12-12 12:41:34 -0300

    refactor: thread factory

--- test/factory/thread.ts
+++ test/factory/thread.ts
@@ -3,40 +3,59 @@ import casual from "casual";
 import { ThreadDocument, Thread, ThreadModel } from "modules/threads";
 import PostFactory from "./post";
 import { PostDocument } from "modules/post";
+import type { UserDocument } from "modules/user";
 
 interface ThreadTransientParams {
 	replies: number;
+	replyAuthor: UserDocument;
+	author: UserDocument;
 }
 
+type ThreadData = Partial<Thread>;
+
 export default Factory.define<
-	Partial<Thread>,
+	ThreadData,
 	ThreadTransientParams,
 	ThreadDocument
 >(({ onCreate, transientParams, associations }) => {
 	onCreate(async thread => {
-		const op = associations?.op ?? (await PostFactory.create())?._id;
-		const threadDoc = new ThreadModel({ ...thread, op });
-		const withReplies = transientParams?.replies ?? 0;
+		const { replies = 0, author, replyAuthor } = transientParams;
+		const op =
+			thread?.op ??
+			(
+				await PostFactory.create(
+					{},
+					{
+						associations: { author: author?._id },
+					}
+				)
+			)?._id;
 
-		if (withReplies > 0) {
-			const thread = await threadDoc.populate<{ op: PostDocument }>("op");
-			const posts = await PostFactory.createList(withReplies);
-			thread.op.children.push(...posts.map(p => p._id));
+		const doc = new ThreadModel({ ...thread, op });
+
+		if (replies > 0) {
+			const [thread, posts] = await Promise.all([
+				doc.populate<{ op: PostDocument }>("op"),
+				PostFactory.createList(
+					replies,
+					{},
+					{ associations: { author: replyAuthor?._id } }
+				),
+			]);
+
+			thread.op.children.push(...posts.map(post => post._id));
 			await thread.op.save();
 		}
 
-		await threadDoc.save();
+		await doc.save();
 
-		return threadDoc;
+		return doc;
 	});
 
 	const thread: Partial<Thread> = {
 		title: casual.title,
+		op: associations?.op,
 	};
-
-	if (associations?.op) {
-		thread.op = associations.op;
-	}
 
 	return thread;
 });
