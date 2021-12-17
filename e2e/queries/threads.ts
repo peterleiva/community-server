cef23844c0b7c94fb9a1b36062b6984b2fd8af76
Peter <github@peter.dev.br>
2021-12-17 18:56:45 -0300

    WIP:  e2e threads graphql query

---
+++ e2e/queries/threads.ts
@@ -0,0 +1,26 @@
+import { GET_THREADS } from "factory";
+import { setupGraphQLServer } from "utils";
+
+describe("threads query", () => {
+	const client = setupGraphQLServer();
+
+	test("empty result when no thread", async () => {
+		const { data } = await client.query({ query: GET_THREADS });
+
+		await expect(data).toMatchObject({
+			threads: {
+				total: 0,
+				edges: [],
+			},
+		});
+	});
+
+	test.todo("gives list of threads");
+	test.todo("no replies gives no participants and no replies");
+	test.todo("count whole replies subtree");
+	test.todo("returns whole likes");
+
+	describe("Paginate", () => {
+		test.todo("no result when first argument is 0");
+	});
+});
