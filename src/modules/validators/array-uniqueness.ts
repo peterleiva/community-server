583465d42a1ef541f9c736013df1a362e8a6879a
Peter <github@peter.dev.br>
2022-01-01 13:18:27 -0300

    styles: improove array uniqueness validator doc

--- src/modules/validators/array-uniqueness.ts
+++ src/modules/validators/array-uniqueness.ts
@@ -1,16 +1,13 @@
 import { ValidateFn, ValidatorMessageFn } from "mongoose";
 
 /**
- * O(n^2) validates whether array has duplicated values, if that's the case
- * returns false
+ * O(1). check whether array has duplicates
  *
- * TODO: Use BST to find duplicated values and improove the complexity
- * @param list array to search duplicated values
- * @return when no duplicated values occurs return true
+ * @param list array to search for duplicates
+ * @return duplicates values occurs return true
  */
 export const validator: ValidateFn<unknown[]> = function validator(list) {
 	const values = new Set<unknown>();
-
 	list.forEach(v => values.add(v));
 
 	return values.size === list.length;
