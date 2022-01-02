eb598e6126a1d9506de73a2b79b920f7d9e5d106
Peter <github@peter.dev.br>
2022-01-02 11:14:11 -0300

    test: add connection shareable sliceable

--- test/shared/connection/paginable.ts
+++ test/shared/connection/paginable.ts
@@ -8,45 +8,7 @@ export default function shouldBehavesLikePaginable(
 	resolver: Resolver
 ) {
 	shouldBehavesLike("paginable", () => {
-		describe("No page argument", () => {
-			describe("More than 20 items", () => {
-				const size = 21;
-
-				beforeEach(async () => await factory(size));
-
-				test("edges has 20 items", async () => {
-					await expect(resolver()).resolves.toBeEdgesOfSize(20);
-				});
-
-				test("pageInfo has next page but no previous", async () => {
-					const result = await resolver();
-
-					expect(result).toHaveNextPage();
-					expect(result).not.toHavePreviousPage();
-				});
-			});
-
-			describe("Less than 20 items", () => {
-				const size = 1;
-
-				beforeEach(async () => await factory(size));
-
-				test("edges has the whole items", async () => {
-					await expect(resolver()).resolves.toBeEdgesOfSize(size);
-				});
-
-				test("pageInfo have no next or previous page", async () => {
-					const result = await resolver();
-
-					expect(result).not.toHaveNextPage();
-					expect(result).not.toHavePreviousPage();
-				});
-			});
-		});
-
-		describe("with page argument", () => {
-			shouldBehavesLikeSliceable(factory, resolver);
-			shouldBehavesLikeSkippable(factory, resolver);
-		});
+		shouldBehavesLikeSliceable(factory, resolver);
+		shouldBehavesLikeSkippable(factory, resolver);
 	});
 }
--- test/shared/connection/sliceable.ts
+++ test/shared/connection/sliceable.ts
@@ -1,6 +1,7 @@
 import { NonNegativeArgument } from "lib";
 import { shouldBehavesLike } from "..";
 import { Resolver, Factory } from "./types";
+import { Page } from "modules/connection";
 import { setupPageArgs } from "test/factory";
 
 export default function shouldBehavesLikeSliceable(
@@ -8,23 +9,40 @@ export default function shouldBehavesLikeSliceable(
 	resolver: Resolver
 ) {
 	shouldBehavesLike("sliceable", () => {
-		describe("Limiting results", () => {
-			test("limit by 'first' argument", async () => {
-				const edges = await factory(2);
+		test("slice to default page size", async () => {
+			await factory(21);
+			const results = await resolver();
+
+			expect(results).toBeFirstPage(Page.DEFAULT_SIZE);
+			expect(results).toHaveNextPage();
+		});
+
+		test("no slicing", async () => {
+			const edges = await factory(2);
+			const result = await resolver();
+
+			expect(result).toBeFirstPage(edges.length);
+			expect(result).not.toHaveNextPage();
+			expect(result).not.toHavePreviousPage();
+		});
+
+		describe("with 'first' argument", () => {
+			test("slicing edges greater than 'first'", async () => {
+				const edges = await factory(3);
 				const limit = 1;
-				const page = setupPageArgs({ first: limit });
+				const page = setupPageArgs({ first: limit, after: edges[0].cursor });
 
 				const result = await resolver(page);
 
-				expect(result).toBeFirstPage(limit);
+				expect(result).toHavePreviousPage();
 				expect(result).toHaveNextPage();
-				expect(result).toHaveCursors({
-					startCursor: edges[0].cursor,
-					endCursor: edges[0].cursor,
+				expect(result).toMatchPageInfo({
+					startCursor: edges[1].cursor,
+					endCursor: edges[1].cursor,
 				});
 			});
 
-			test("negative limit throws NonNegativeArgument", async () => {
+			test("negative value throws NonNegativeArgument", async () => {
 				const page = setupPageArgs({ first: -1 });
 
 				await expect(resolver(page)).rejects.toThrowError(NonNegativeArgument);
@@ -44,21 +62,12 @@ export default function shouldBehavesLikeSliceable(
 				const result = await resolver(page);
 
 				expect(result).toBeFirstPage(edges.length);
-				expect(result).toHaveCursors({
+				expect(result).toMatchPageInfo({
 					startCursor: edges[0].cursor,
 					endCursor: edges[1].cursor,
 				});
 				expect(result).not.toHaveNextPage();
-			});
-
-			test("limit with edges greater than 'first'", async () => {
-				const edges = await factory(3);
-				const args = setupPageArgs({
-					first: 1,
-					after: edges[1].cursor,
-				});
-
-				await expect(resolver(args)).resolves.toBeEdgesOfSize(1);
+				expect(result).not.toHavePreviousPage();
 			});
 		});
 	});
